0d1a88f6c82423897d73cbbcaf591cd2f14b9a78
Rodolfo Carlos <luis.carlos@dxc.com>
2022-03-01 10:02:57 -0700

    Added routing and styling to main component

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,24 @@ import NavBar from './components/Menu';
 import ComicsPage from './components/ComicsPage';
 import CharactersPage from './components/CharactersPage';
 import NotFound from './components/NotFound';
+import ComicDetailsPage from './components/ComicDetailsPage';
+import CharacterDetailsPage from './components/CharacterDetailsPage';
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-      <Routes>
-        <Route path="/comics" element={<ComicsPage />} />
-        <Route path="/characters" element={<CharactersPage />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <div className="container">
+        <Routes>
+          <Route path="/comics" element={<ComicsPage />} />
+          <Route path="/" element={<ComicsPage />} />
+          <Route path="/characters" element={<CharactersPage />} />
+          <Route path="/comics/:id" element={<ComicDetailsPage />} />
+          <Route path="/characters/:id" element={<CharacterDetailsPage />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
     </div>
   );
 }
